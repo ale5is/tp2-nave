af9b5ed69e020fe181563aec9d65f674e5b1baef
ale5is <alexiscapricornio14@gmail.com>
2024-05-12 12:53:42 -0300

    BUG FIX RESTART

--- Assets/Scripts/Controller_Player.cs
+++ Assets/Scripts/Controller_Player.cs
@@ -39,6 +39,8 @@ public class Controller_Player : MonoBehaviour
     
     private void Awake()
     {
+        Time.timeScale = 1;
+
         if (_Player == null)
         {
             _Player = GameObject.FindObjectOfType<Controller_Player>();
